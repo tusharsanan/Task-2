386e53cabc43e94331124961038340defee4d2da
TusharSanan <tushar.sanan@gmail.com>
2017-06-12 22:46:31 +0530

    Adding Assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component ({  
+    selector: 'app-root',
+    template: `
+    <h1>{{title}}</h1>
+    <nav>
+        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+    `,
+    styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent {
-  title = 'app works!';
+    title = 'Tour of Heroes';
 }
